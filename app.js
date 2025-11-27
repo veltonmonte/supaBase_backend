99a6c39c114be9d8f77c8c2c6f5e886109f782f2
veltonmonte <montevelton@gmail.com>
2025-11-27 00:14:30 -0300

    Refactor app.js to use constants for Supabase URL and key

--- app.js
+++ app.js
@@ -35,10 +35,11 @@ app.use(morgan('combined'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-const supabase = 
-    supabaseClient.createClient('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJwaXFmbXJ3bGZkdW1tdGZqZGV1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI5OTgyMzQsImV4cCI6MjA3ODU3NDIzNH0._iuQBg2K1DxGoB0yAqtiYdnw4z2pFnJTHaOx9cNTCog', 
-        'https://rpiqfmrwlfdummtfjdeu.supabase.co')
+const supaBaseURL = "https://rpiqfmrwlfdummtfjdeu.supabase.co";
+const supaBaseKEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJwaXFmbXJ3bGZkdW1tdGZqZGV1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI5OTgyMzQsImV4cCI6MjA3ODU3NDIzNH0._iuQBg2K1DxGoB0yAqtiYdnw4z2pFnJTHaOx9cNTCog";
 
+const supabase = 
+    supabaseClient.createClient(supaBaseURL, supaBaseKEY)
 
 app.get('/products', async (req, res) => {
     const {data, error} = await supabase
